81820ec8e5f39319f22c5ed41112d8b213dccfb7
Sai Sreenivas <sreenivassaibks@gmail.com>
2023-03-06 12:24:39 +0530

    this is developer 2 commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ## this is is list of git lists by me 
 ## this commints
+## this Amazing tutorials by devlp 2 
--- app.py
+++ app.py
@@ -1,3 +1,4 @@
 print("Hello")
 print("Second statement")
 print("ultimate done ")
+print("this is developer 222")
