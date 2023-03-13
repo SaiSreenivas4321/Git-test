46e87095a401970b34dabede0c5fdb7b490d3ec3
Sai Sreenivas <sreenivassaibks@gmail.com>
2023-03-13 23:06:18 +0530

    this ara the changes in branch 2

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## this is is list of git lists by me 
 ## this commints
 ## this Amazing tutorials by devlp 2 
+## this si amazing by developer2 
--- app.py
+++ app.py
@@ -2,3 +2,5 @@ print("Hello")
 print("Second statement")
 print("ultimate done ")
 print("this is developer 222")
+
+print("Createed my new branch ")
